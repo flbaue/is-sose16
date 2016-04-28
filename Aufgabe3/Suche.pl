b7d44a4e7a0d9ddf9028b1982230969df35d5ace
MariaAI <you@example.com>
2016-04-28 08:37:31 +0200

    aufgabe3

---
+++ Aufgabe3/Suche.pl
@@ -0,0 +1,9 @@
+
+:- consult('Suche_Modul_Allgemein.pl').
+:- consult('Suche_Modul_Planung_Vorlage.pl').
+
+%:- consult('Suche_Modul_Informierte_Suche.pl').
+
+%%% Spezieller Teil: Planung
+%:- consult('Suche_Modul_Planung.pl').
+
