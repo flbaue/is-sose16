3e087d7eb8a93dac4e691ddd45c727af21e57b9d
flbaue <florian.bauer@posteo.de>
2016-06-16 11:05:15 +0200

    added javadoc

--- ConstraintSolver/src/isp/a4/ConstraintSolver.java
+++ ConstraintSolver/src/isp/a4/ConstraintSolver.java
@@ -5,7 +5,6 @@ import isp.a4.data.Kante;
 import isp.a4.data.Knoten;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * Created by florian on 04.06.2016.
@@ -14,21 +13,43 @@ public class ConstraintSolver {
     public List<Knoten> knoten = new ArrayList<>();
     private Set<Kante> kanten = new HashSet<>();
 
-    public Knoten addVariable(String name, List<Integer> dom) {
-        Knoten k = new Knoten(name, dom);
+    /**
+     * Erstellt eine Variable und speichert sie in einer Liste
+     * @param name der Variable
+     * @param domain der Variable
+     * @return die Variable
+     */
+    public Knoten addVariable(String name, List<Integer> domain) {
+        Knoten k = new Knoten(name, domain);
         knoten.add(k);
         return k;
     }
 
+    /**
+     * Fügt einen gerichteten Constraint hinzu.
+     * @param from Variable
+     * @param to Varible
+     * @param constraint
+     */
     public void addUnaryConstraint(Knoten from, Knoten to, Constraint constraint) {
         kanten.add(new Kante(from, to, constraint));
     }
 
+    /**
+     * Fügt beidseitigen Constraint hinzu. Wird durch zwei gerichtete abgebildet.
+     * @param a Variable
+     * @param b Variable
+     * @param constraint
+     */
     public void addBiConstraint(Knoten a, Knoten b, Constraint constraint) {
         addUnaryConstraint(a, b, constraint);
         addUnaryConstraint(b, a, constraint);
     }
 
+    /**
+     * Fügt constraints hinzu, so dass alle gegebenen Variablen unterschiedlich sein müssen.
+     * @param knoten Menge von Variablen
+     */
     public void allDifferent(Collection<Knoten> knoten) {
         for (Knoten elem : knoten) {
             for (Knoten elem2 : knoten) {
@@ -39,8 +60,13 @@ public class ConstraintSolver {
         }
     }
 
+    /**
+     * AC-3 mit Look a Head
+     * @param cv Nummer der aktuell belegten Variable
+     * @return konsistent oder nicht
+     */
     public boolean ac3LA(int cv) {
-        Queue<Kante> queue = addKorrekteKanten(cv);
+        Queue<Kante> queue = findeKanten(cv);
         boolean consistent = true;
         while (!queue.isEmpty() && consistent) {
             Kante delete = queue.poll();
@@ -58,6 +84,11 @@ public class ConstraintSolver {
         return consistent;
     }
 
+    /**
+     * Rekursive Methode welche jeweils die aktuelle Variable belegt und mit AC-3 LaH überprüft.
+     * @param currentIndex der aktuellen Variable (aus der Liste)
+     * @return false wenn es keine konsistente Lösung gibt. Sonst true, Lösung ist der aktuelle zustand der Variablen.
+     */
     public boolean solve(int currentIndex) {
         if (knoten.get(currentIndex).getDomain().isEmpty()) {
             return false;
@@ -92,6 +123,11 @@ public class ConstraintSolver {
         return false;
     }
 
+    /**
+     * Erstellt Kopien aller Domain-Listen der Variablen in einer Map
+     * @param knoten Variablen
+     * @return Map mit Domain-Kopien und Variablenreferenz
+     */
     private Map<Knoten, List<Integer>> backupDomains(List<Knoten> knoten) {
         Map<Knoten, List<Integer>> domains = new HashMap<>();
 
@@ -103,6 +139,10 @@ public class ConstraintSolver {
         return domains;
     }
 
+    /**
+     * Setzt die Domains der gegebenen Variablen wieder auf den gesicherten Zustand zurück.
+     * @param domains
+     */
     private void resetDomains(Map<Knoten, List<Integer>> domains) {
         for (Map.Entry<Knoten, List<Integer>> e : domains.entrySet()) {
             Knoten k = e.getKey();
@@ -110,22 +150,12 @@ public class ConstraintSolver {
         }
     }
 
-    public void checkUnary() {
-        final List<Kante> unaryConstraints = kanten.stream().filter(edge -> {
-            return edge.getStart().equals(edge.getEnde());
-        }).collect(Collectors.toCollection(ArrayList::new));
-
-        for (Kante k : unaryConstraints) {
-            List<Integer> dom = k.getStart().getDomain();
-            List<Integer> result = new ArrayList<>();
-            for (Integer i : dom) {
-                if (ConstraintChecker.check(k.getConstraint(), i, i)) result.add(i);
-            }
-            k.getStart().setDomain(result);
-        }
-    }
-
-    private Queue<Kante> addKorrekteKanten(int cv) {
+    /**
+     * Findet alle Kanten die zur Variablen mit dem gegeben Index gehören
+     * @param cv Index
+     * @return Kanten
+     */
+    private Queue<Kante> findeKanten(int cv) {
         Queue<Kante> result = new LinkedList<>();
         for (Kante k : kanten) {
             if (knoten.indexOf(k.getStart()) > cv && knoten.indexOf(k.getEnde()) == cv) {
@@ -135,6 +165,11 @@ public class ConstraintSolver {
         return result;
     }
 
+    /**
+     * Prüft und reduziert die Variablen-Domain nach dem gegebenen Constraint
+     * @param edge Constraint
+     * @return Ob eine Reduzierung stattgefunden hat
+     */
     private boolean revise(Kante edge) {
         boolean action = false;
         // To avoid Concurrent Modification Error:
@@ -148,6 +183,12 @@ public class ConstraintSolver {
         return action;
     }
 
+    /**
+     * Prüft ob konkrete Domain-Werte möglich sind
+     * @param edge Constraint
+     * @param value Wert
+     * @return
+     */
     private boolean isValuePossible(Kante edge, Integer value) {
         for (Integer value2 : edge.getEnde().getDomain()) {
             if (ConstraintChecker.check(edge.getConstraint(), value, value2)) {
